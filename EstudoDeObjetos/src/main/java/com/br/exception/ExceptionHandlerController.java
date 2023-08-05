dc2f362b542c9f8c49dc70b8d9fa38d88d0d9586
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2023-08-05 00:04:13 -0300

    adicionado validation

---
+++ EstudoDeObjetos/src/main/java/com/br/exception/ExceptionHandlerController.java
@@ -0,0 +1,33 @@
+package com.br.exception;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.br.model.ResponseModel;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    
+	/**
+	 * @author João	
+	 * @apiNote Método que retorna erros do validation
+	 */
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    	ResponseModel response = new ResponseModel();
+		List<String> erros = ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage)
+				.collect(Collectors.toList());	
+		
+		for (String listaErro : erros) {
+			response.setMessage(listaErro);
+		}
+
+		return new ResponseEntity<>(erros, HttpStatus.BAD_REQUEST);		
+    }
+}
