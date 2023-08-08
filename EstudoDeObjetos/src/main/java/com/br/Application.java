81f13ec89d399b87c27db47ef3ce3f456304b1d6
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2023-08-08 11:30:05 -0300

    criado função que chama o swagger ao iniciar aplicação.

--- EstudoDeObjetos/src/main/java/com/br/Application.java
+++ EstudoDeObjetos/src/main/java/com/br/Application.java
@@ -1,29 +1,33 @@
 package com.br;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
-@EntityScan("com.br.model") // Escaneia o pacote com as entidades JPA
-@EnableJpaRepositories("com.br.repository") // Configuração do pacote com os repositórios JPA
-@EnableScheduling
+@EntityScan("com.br.model")
+@EnableJpaRepositories("com.br.repository")
 public class Application {
-	
+
 	@Bean
-	public ModelMapper modelMapper(){
+	public ModelMapper modelMapper() throws Exception {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setSkipNullEnabled(true);
 		return modelMapper;
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws Exception {
+		String swaggerUrl = "http://localhost:8080/swagger-ui.html#/";
+	
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			SpringApplication.run(Application.class, args);
+			Desktop.getDesktop().browse(new URI(swaggerUrl));		
+		}
 	}
-
 }
