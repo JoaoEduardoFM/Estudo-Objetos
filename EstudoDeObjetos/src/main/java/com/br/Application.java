bb71197fc23d8c3f8694f97d559e9ca7428210f5
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2023-08-16 19:28:43 -0300

    corrigido erro ao inicializar e adicionado função que inicializa swagger

--- EstudoDeObjetos/src/main/java/com/br/Application.java
+++ EstudoDeObjetos/src/main/java/com/br/Application.java
@@ -4,20 +4,19 @@ import java.awt.Desktop;
 import java.net.URI;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EntityScan("com.br.model")
 @EnableJpaRepositories("com.br.repository")
 public class Application {
-
-    @Value("${custom.swaggerUrl}") // Lê a propriedade do arquivo YAML
-    private String swaggerUrl;
 
     @Bean
     public ModelMapper modelMapper() throws Exception {
@@ -27,11 +26,13 @@ public class Application {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-        
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-            // Acessar a nova URL do Swagger que foi lida do YAML
-            Desktop.getDesktop().browse(new URI(args[0])); // Passa a URL como argumento
-        }
+    	String swaggerUrl = "http://localhost:8080/swagger-ui.html#/";
+
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			SpringApplication.run(Application.class, args);
+			Desktop.getDesktop().browse(new URI(swaggerUrl));
+			log.info("Aplicação iniciada.");
+
+		}
     }
 }
